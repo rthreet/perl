646044c68eb1725f89759c3ee21755c977df084c
Robert Threet <rthreet@usi.edu>
2019-06-28 16:09:49 -0500

    Ran Perl::Tidy

--- mkSnap.pl
+++ mkSnap.pl
@@ -8,29 +8,30 @@
 
 use POSIX qw(strftime);
 
-$UUID = "fd5cd464-33f9-708e-87ea-4de3a24d7313"; # Exchhub
+$UUID = "fd5cd464-33f9-708e-87ea-4de3a24d7313";    # Exchhub
 $host = "exchhub";
 
 # Capture snapshot UUID here
 $log = "/var/log/snapshot.log";
-open(LOG,">>$log") or die "Cannot open $log: $!\n";
+open( LOG, ">>$log" ) or die "Cannot open $log: $!\n";
 
 # grab short day of week name
 $month = strftime "%b", localtime;
+
 # grab hour of day
 $year = strftime "%Y", localtime;
+
 # Grab time stamp for log
 $logtime = strftime "%Y:%b:%e:%H:%M:%S", localtime;
 
 $snapName = $host . $month . $year . "snap";
 
-# -with-quiesce Quiesced snapshots take advantage of the 
-# Windows Volume Shadow Copy Service (VSS) to generate 
+# -with-quiesce Quiesced snapshots take advantage of the
+# Windows Volume Shadow Copy Service (VSS) to generate
 # application consistent point-in-time snapshots. (Did not seem to work)
 # my $value = qx(xe vm-snapshot-with-quiesce uuid=$UUID new-name-label=$snapName 2>&1);
 my $value = qx(xe vm-snapshot uuid=$UUID new-name-label=$snapName 2>&1);
 
 # Capture snapshot UUID stored here (/var/log/snapshot.log)
 print LOG "$logtime:$snapName:$value";
-
 
