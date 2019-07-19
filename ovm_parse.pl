cd3da7364aabab976d147d4b09a5307af862aee2
Robert Threet <rthreet@usi.edu>
2019-07-19 11:25:14 -0500

    Good start on OVM Report parser.

--- ovm_parse.pl
+++ ovm_parse.pl
@@ -1,10 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # RAT 7/19/19
 # New approach to reporting on OVM
 #
-
-use 5.010;
-use warnings;
 
 # Best XML module I've ever used!
 use XML::LibXML;
@@ -14,21 +11,22 @@ my $ovmreport = 'OVM_Report.xml';
 
 $count = 0;
 
-my $dom = XML::LibXML->load_xml(location => $ovmreport) or die "Nope:$!\n";
+my $dom = XML::LibXML->load_xml( location => $ovmreport ) or die "Nope:$!\n";
 
-foreach my $server ($dom->findnodes('//OvmReport/Vm')) {
+foreach my $server ( $dom->findnodes('//OvmReport/Vm') ) {
     my $state = $server->findvalue('./vmRunState');
+
     # Report only RUNNING (exclude snapshots and STOPPED)
-    if ($state =~ m/RUNNING/) {
-    	say 'Name:          ', $server->findvalue('./id/name');
-    	say 'CPUs:          ', $server->findvalue('./cpuCount');
-    	say 'Memory:        ', $server->findvalue('./memory');
-    	say 'OS Type:       ', $server->findvalue('./osType');
-    	say 'Repository:    ', $server->findvalue('./repositoryId/name');
-    	say 'Virtual NICs:  ', $server->findvalue('./vnics/vnic/ipAddresses');
-    	#say 'Virtual Disks  ', $server->findvalue('./vmDiskMappings/vmDiskMapping/virtualDisk/size');  # This doesn't work
-	$count++;
-	print "---\n";
+    if ( $state =~ m/RUNNING/ ) {
+        print 'Name:       ', $server->findvalue('./id/name'),  "\n";
+        print 'CPUs:       ', $server->findvalue('./cpuCount'), "\n";
+        print 'Memory:     ', $server->findvalue('./memory'),   "\n";
+        print 'OS Type:    ', $server->findvalue('./osType'),   "\n";
+        print 'Repository: ', $server->findvalue('./repositoryId/name'), "\n";
+        print 'Virtual NICs:  ',
+          $server->findvalue('./vnics/vnic/ipAddresses'), "\n";
+        $count++;
+        print "---\n";
     }
 }
 print "Count: $count\n";
